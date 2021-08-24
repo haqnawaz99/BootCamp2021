169d3e995466ce6ea141882ec8e611dd81630cd7
Haq Nawaz <haqnawaz99@yahoo.com>
2021-08-24 19:52:00 +0500

    map iplemented

--- UrduClass05/dist/hello.js
+++ UrduClass05/dist/hello.js
@@ -1,2 +1,8 @@
-console.clear();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Musa Todo List");
+const TaskCollection_1 = require("./TaskCollection");
+let task01 = new TaskCollection_1.TaskColection();
+task01.addTodo("Medicine");
+console.clear();
+task01.printAll();
